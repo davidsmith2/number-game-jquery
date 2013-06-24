eee6e17590b5a6d8f02ed52e11422c91e0e68b2f
David Smith <davidsmith2@gmail.com>
2013-06-24 14:57:26 -0400

    added profile and leaderboard screens

--- nu.js
+++ nu.js
@@ -1,66 +1,67 @@
-/* GAME PROPERTIES */
-function Game () {
+/* CLASSES */
+function Game (playerName, guessesAllowed) {
+	this.playerName = playerName;
 	this.guess = null;
 	this.guessAccuracy = null;
-	this.guessesAllowed = null;
+	this.guessesAllowed = guessesAllowed;
 	this.guessesMade = 0;
 	this.guessesRemaining = null;
+	this.getPlayerName = function () {
+		return this.playerName;
+	};
+	this.getGuess = function () {
+		return this.guess;
+	};
+	this.setGuess = function (guess) {
+		this.guess = guess;
+	};
+	this.writeGuess = function (guess) {
+		$('#guess > span.value').empty().append(guess);
+	};
+	this.getGuessAccuracy = function () {
+		return this.guessAccuracy;
+	};
+	this.setGuessAccuracy = function (guessAccuracy) {
+		this.guessAccuracy = guessAccuracy;
+	};
+	this.writeGuessAccuracy = function (guessAccuracy) {
+	    var target, className;
+	    target 		= $('#guess-accuracy > span.value');
+	    className 	= guessAccuracy.toLowerCase();
+	    target.empty().removeClass().addClass('value ' + className).append(guessAccuracy);
+	};
+	this.getGuessesAllowed = function () {
+		return this.guessesAllowed;
+	};
+	this.setGuessesAllowed = function (guessesAllowed) {
+		this.guessesAllowed = guessesAllowed;
+	};
+	this.writeGuessesAllowed = function (guessesAllowed) {
+		$('#guesses-allowed > span.value').empty().append(guessesAllowed);
+	};
+	this.getGuessesMade = function () {
+		return this.guessesMade;
+	};
+	this.setGuessesMade = function (guessesMade) {
+		this.guessesMade = guessesMade;
+	};
+	this.writeGuessesMade = function (guessesMade) {
+		$('#guesses-made > span.value').empty().append(guessesMade);
+	},
+	this.getGuessesRemaining = function () {
+		return this.guessesAllowed - this.guessesMade;
+	};
+	this.setGuessesRemaining = function (guessesRemaining) {
+		this.guessesRemaining = guessesRemaining;
+	};
+	this.writeGuessesRemaining = function (guessesRemaining) {
+		$('#guesses-remaining > span.value')
+			.empty()
+			.append(guessesRemaining);
+	};
 }
-/* GAME METHODS (normal) */
-Game.prototype.getGuess = function () {
-	return this.guess;
-};
-Game.prototype.setGuess = function (guess) {
-	this.guess = guess;
-};
-Game.prototype.getGuessAccuracy = function () {
-	return this.guessAccuracy;
-};
-Game.prototype.setGuessAccuracy = function (guessAccuracy) {
-	this.guessAccuracy = guessAccuracy;
-};
-Game.prototype.getGuessesAllowed = function () {
-	return this.guessesAllowed;
-};
-Game.prototype.setGuessesAllowed = function (guessesAllowed) {
-	this.guessesAllowed = guessesAllowed;
-};
-Game.prototype.getGuessesMade = function () {
-	return this.guessesMade;
-};
-Game.prototype.setGuessesMade = function (guessesMade) {
-	this.guessesMade = guessesMade;
-};
-Game.prototype.getGuessesRemaining = function () {
-	return this.guessesAllowed - this.guessesMade;
-};
-Game.prototype.setGuessesRemaining = function (guessesRemaining) {
-	this.guessesRemaining = guessesRemaining;
-};
-/* GAME METHODS (writing) */
-Game.prototype.writeGuess = function (guess) {
-	$('#guess > span.value').empty().append(guess);
-};
-Game.prototype.writeGuessAccuracy = function (guessAccuracy) {
-    var target, className;
-    target 		= $('#guess-accuracy > span.value');
-    className 	= guessAccuracy.toLowerCase();
-    target.empty().removeClass().addClass('value ' + className).append(guessAccuracy);
-};
-Game.prototype.writeGuessesAllowed = function (guessesAllowed) {
-	$('#guesses-allowed > span.value').empty().append(guessesAllowed);
-};
-Game.prototype.writeGuessesMade = function (guessesMade) {
-	$('#guesses-made > span.value').empty().append(guessesMade);
-},
-Game.prototype.writeGuessesRemaining = function (guessesRemaining) {
-	$('#guesses-remaining > span.value')
-		.empty()
-		.append(guessesRemaining);
-};
 /* APP */
 var nu = {
-    isRunning: false,
     settings: {
 		lowTile: 1,
 		highTile: 100,
@@ -107,14 +108,13 @@ var nu = {
 		    settings	= nu.settings,
 		    state 		= nu.state,
 		    ui 			= nu.ui,
-		    overlays	= ui.overlays,
-		    guessesAllowed;
-		nu.isRunning = true;
+		    overlays	= ui.overlays;
 		prepareTiles();
 		prepareDialogs();
 		prepareDialogTriggers();
 		preparePlayButton();
 		prepareSettings();
+		prepareProfile();
 		// prepare the game board tiles
 		function prepareTiles () {
 			var tiles = '',
@@ -158,17 +158,19 @@ var nu = {
 		}
 		// the result of pressing "play"!
 		function startGame () {
-			var game, guessesAllowed, secretNumber, link, result;
+			var game,
+				result,
+				secretNumber,
+				tileLink;
 			$('.tile').removeClass('visited match');
-			game = new Game();
-			game.setGuessesAllowed(findGuessesAllowed());
+			game = new Game(findPlayerName(), findGuessesAllowed());
 			secretNumber = generateSecretNumber();
 			$('a.tile').unbind('click').bind('click', function (e) {
-			    var link = $(this);
-			    link.addClass('visited').parent('div').addClass('visited');
-				result = handleGuess(game, link, secretNumber);
+			    var tileLink = $(this);
+			    tileLink.addClass('visited').parent('div').addClass('visited');
+				result = handleGuess(game, tileLink, secretNumber);
 				if (typeof result !== "undefined") {
-					showResult(secretNumber, result, link);
+					showResult(secretNumber, result, tileLink);
 					resetGame(game);
 				}
 				updateGauges(game);
@@ -176,13 +178,14 @@ var nu = {
 			});
 			prepareGauges(game);
 		}
-		// get the current number of guesses allowed from the settings form
+		// get the player's name from the profile form
+		function findPlayerName () {
+	    	var profileFormName = $("#profile-form-name");
+	    	return (profileFormName.val() !== '') ? profileFormName.val() : profileFormName.attr('placeholder');
+		}
+		// get the number of guesses the player is allowed from the settings form
 		function findGuessesAllowed () {
-		    var form, currentValue, newValue;
-		    form = $('#settings-form');
-		    currentValue = form.find('input[type=radio]').eq(0).val();
-		    newValue = form.find('input[checked]').val();
-			return (typeof newValue !== "undefined") ? newValue : currentValue;
+	    	return $('#settings-form').find('input[checked]').attr('value');
 		}
 		// generate the random secret number
 		// (see StackOverflow question 1527803)
@@ -192,17 +195,17 @@ var nu = {
 			return Math.floor(Math.random() * (highTile - lowTile + 1)) + lowTile;
 		}
 		// ask some pertinent questions about the current guess
-		function handleGuess (game, link, secretNumber) {
+		function handleGuess (game, tileLink, secretNumber) {
 			// how many guesses does the player have left?
 			game.setGuessesMade(game.getGuessesMade() + 1);
 			game.setGuessesRemaining(game.getGuessesAllowed() - game.getGuessesMade());
 			// what is the current guess?
-			game.setGuess(parseInt(link.text()));
+			game.setGuess(parseInt(tileLink.text()));
 			// is the current guess right or wrong?
 			if (game.getGuess() !== secretNumber) {
-    			// if the current guess is wrong and 
-    			// the player has no guesses left, 
-    			// they lose
+				// if the current guess is wrong and 
+				// the player has no guesses left, 
+				// they lose
 			    if (game.getGuessesRemaining() === 0) return 'lose';
 			    // if the current guess is wrong and
 			    // the player has some guesses left, 
@@ -211,7 +214,7 @@ var nu = {
 			} else {
 				// if the current guess is right,
 				// the player wins
-				return handleRightGuess(game, link);
+				return handleRightGuess(game, tileLink);
 			}
 		}
 		// did the player guess wrong
@@ -221,25 +224,25 @@ var nu = {
 				guessAccuracy = ui.strings.lowGuess; // did the player guess low
 			} else if (guess > secretNumber) {
 				guessAccuracy = ui.strings.highGuess; // did the player guess high
-
 			}
 			game.setGuessAccuracy(guessAccuracy);
 		}
 		// did the player guess right?
-		function handleRightGuess (game, link) {
+		function handleRightGuess (game, tileLink) {
 			game.setGuessAccuracy(ui.strings.rightGuess);
-			link
+			tileLink
 				.removeClass('visited')
 				.parent('div')
 				.removeClass('visited');
-			link.addClass('match')
+			tileLink.addClass('match')
 				.parent('div')
 				.addClass('match');
+			isGameLeaderboardWorthy(game);
 			return 'win';
 		}
 		// open the win/lose dialog if the result of the game is known
-		function showResult (secretNumber, result, link) {
-			link
+		function showResult (secretNumber, result, tileLink) {
+			tileLink
 			    .unbind('click')
 				.attr('rel', '#' + result)
 				.overlay(overlays.attributes.autoload);
@@ -250,24 +253,19 @@ var nu = {
 				.find('span.secret-number')
 				.empty()
 				.append(secretNumber);
-			
-		}
-		// nullify the game instance when the game is over
-		function resetGame (game) {
-			game = null;
-		}
-		// update the game board gauges each time a guess is made
-        function updateGauges (game) {
-			game.writeGuess				(game.getGuess());
-			game.writeGuessAccuracy		(game.getGuessAccuracy());
-			game.writeGuessesMade		(game.getGuessesMade());
-			game.writeGuessesRemaining	(game.getGuessesRemaining());
 		}
 		// prepare the game board gauges
 		function prepareGauges (game) {
 			game.writeGuess				('-');
 			game.writeGuessAccuracy		('-');
 			game.writeGuessesAllowed	(game.getGuessesAllowed());
+			game.writeGuessesMade		(game.getGuessesMade());
+			game.writeGuessesRemaining	(game.getGuessesRemaining());
+		}
+		// update the game board gauges each time a guess is made
+        function updateGauges (game) {
+			game.writeGuess				(game.getGuess());
+			game.writeGuessAccuracy		(game.getGuessAccuracy());
 			game.writeGuessesMade		(game.getGuessesMade());
 			game.writeGuessesRemaining	(game.getGuessesRemaining());
 		}
@@ -280,7 +278,6 @@ var nu = {
 			$('#play').expose();
 			// hide the mask
 			$.mask.close();
-			
 		}
 		// open overlay (does what it says on the tin!)
 		function openOverlay (id) {
@@ -318,10 +315,6 @@ var nu = {
 			setAttribute(input, 'checked', 'checked');
 			return guessesAllowed;
 		}
-		// a generic function for setting an element's attribute value
-		function setAttribute (element, attribute, value) {
-			element.attr(attribute, value);
-		}
 		// get the new number of guesses allowed
 		function confirmSettingChange (form) {
 			return parseInt(form.find('input[checked]').val());
@@ -330,6 +323,64 @@ var nu = {
 		function cancelSettingChange (form, oldSetting) {
 			form.find('input[checked]').removeAttr('checked');
 			form.find('input[value=' + oldSetting + ']').attr('checked', 'checked');
+		}
+		// determine the player's name
+		function prepareProfile () {
+			var form,
+				textBox,
+			    buttons;
+			form = $('#profile-form');
+			textBox = $('#profile-form-name');
+			buttons = $('#profile .buttons');
+			buttons.find('a').eq(0).bind('click', function (e) {
+				textBox.attr('placeholder', textBox.val());
+				e.preventDefault();
+			});
+			buttons.find('a').eq(1).bind('click', function (e) {
+				textBox.val(textBox.attr('placeholder'));
+				e.preventDefault();
+			});
+		}
+		// does the current game warrant an entry on the leaderboard?
+		function isGameLeaderboardWorthy (game) {
+	    	var guessesMade, positionOnLeaderboard;
+	    	guessesMade = game.getGuessesMade();
+	    	positionOnLeaderboard = checkLeaderboard(guessesMade);
+	    	if (positionOnLeaderboard !== "undefined") {
+	    		updateLeaderBoard(positionOnLeaderboard, game);
+	    	}
+		}
+	    // check where the current game belongs on the leaderboard
+	    function checkLeaderboard (newGuessesMade) {
+	    	var position, rows;
+	    	rows = $('#leaderboard-table > tbody').find('tr');
+	    	$.each(rows, function (index) {
+	    		var oldGuessesMade = parseInt($(this).find('td').eq(2).text());
+	    		if (newGuessesMade < oldGuessesMade) {
+					position = index;
+					return false;
+	    		}
+	    	});
+	    	return position;
+	    }
+	    // update the leaderboard if the current game warrants an entry
+	    function updateLeaderBoard (positionOnLeaderboard, game) {
+	    	var leaderboardTableBody,
+	    		rowToAdd,
+	    		rowToTarget;
+	    	leaderboardTableBody = $('#leaderboard-table > tbody');
+	    	rowToAdd = $('<tr><td>' + game.getPlayerName() + '</td><td>' + game.getGuessesAllowed() + '</td><td>' + game.getGuessesMade() + '</td></tr>');
+	    	rowToTarget = leaderboardTableBody.find('tr').eq(positionOnLeaderboard);
+			rowToAdd.insertBefore(rowToTarget);
+			leaderboardTableBody.find('tr').eq(3).remove();
+	    }
+		// nullify the game instance when the game is over
+		function resetGame (game) {
+			game = {};
+		}
+		// a generic function for setting an element's attribute value
+		function setAttribute (element, attribute, value) {
+			element.attr(attribute, value);
 		}
     }
 };
